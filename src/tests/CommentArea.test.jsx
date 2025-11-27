703dfe3b3ca622f07023badfb7c39bb34156ff86
NAD3R <naderdeghaili@gmail.com>
2025-11-27 16:01:24 +0100

    fixed order

--- src/tests/CommentArea.test.jsx
+++ src/tests/CommentArea.test.jsx
@@ -3,19 +3,21 @@ import CommentArea from "../components/CommentArea"
 import { describe, expect, it } from "vitest"
 
 describe("Testing correct rendering of Comment Area", () => {
-  it("checks if it renders CommentList correctly ", () => {
+  it("checks if it renders loading correctly", () => {
     render(<CommentArea asin={1234} />)
-    const commentList = screen.getByTestId("comment-list")
-    expect(commentList).toBeInTheDocument()
+    const spinner = screen.getByTestId("loading-spinner")
+    expect(spinner).toBeInTheDocument()
   })
+
   it("checks if it renders input area correctly ", () => {
     render(<CommentArea asin={1234} />)
     const input = screen.getByPlaceholderText(/inserisci/i)
     expect(input).toBeInTheDocument()
   })
-  it("checks if it renders loading correctly", () => {
+
+  it("checks if it renders CommentList correctly ", () => {
     render(<CommentArea asin={1234} />)
-    const spinner = screen.getByTestId("loading-spinner")
-    expect(spinner).toBeInTheDocument()
+    const commentList = screen.getByTestId("comment-list")
+    expect(commentList).toBeInTheDocument()
   })
 })
